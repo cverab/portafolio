aac1f0154376867de4c026b8021789dce65c13e8
Carlos Vera <cverab@outlook.com>
2023-08-04 00:20:57 -0500

    Validación de formulario e Iconos FontAwesome

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import { validar } from "./validador.js"
+import { validarTextarea } from "./validador.js"
+
+const inputs = document.querySelectorAll(`[data-tipo]`);
+
+inputs.forEach(input => {
+    input.addEventListener("blur", (input) => {
+        validar(input.target)
+        validarTextarea()
+    })
+});
+
+
---
+++ js/validador.js
@@ -0,0 +1,55 @@
+export function validar(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (input.validity.valid) {
+        input.parentElement.querySelector(".contacto__formulario-error").innerHTML = ""
+    } else {
+        input.parentElement.querySelector(".contacto__formulario-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input)
+    }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError"
+];
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo nombre no púede estar vacio",
+        patternMismatch: "El campo nombre debe contener al menos 1 caracteres y máximo 50."
+    },
+    correo: {
+        valueMissing: "El campo correo no púede estar vacio",
+        typeMismatch: "El campo correo no es válido"
+    },
+    asunto: {
+        valueMissing: "El campo asunto no debe estar en blanco o vacío",
+        patternMismatch: "El campo asunto debe contener al menos 1 caracteres y máximo 50."
+    },
+    msj: {
+        valueMissing: "El campo mensaje no debe estar en blanco o vacío",
+        customError: "El campo mensaje debe contener al menos 1 caracteres y máximo 300."
+    },
+}
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = ""
+    tipoDeErrores.forEach(error => {
+        if (input.validity[error]) {
+            mensaje = mensajesDeError[tipoDeInput][error];
+        };
+    });
+    return mensaje
+}
+
+export function validarTextarea() {
+    const textarea1 = document.getElementById("textarea").value;
+    const textarea2 = textarea1.length;
+    if (textarea2 >= 300) {
+        console.log(textarea2)
+    }
+    else {
+        console.log(textarea2)
+    }
+}
