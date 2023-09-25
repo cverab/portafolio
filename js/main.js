caf47197b9eb5a710c8ef6350d04619640dae367
Carlos Vera <cverab@outlook.com>
2023-09-24 23:23:03 -0500

    corregido estilos, agregando datos

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ inputs.forEach(input => {
         validarTextarea(input.target)
     })
 });
+
+const btn = document.querySelector(".contacto__formulario-texto-boton");
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const subj = document.getElementById("subj");
+    const messaje = document.getElementById("textarea");
+
+    if (name.validity.valid && email.validity.valid && subj.validity.valid && messaje.validity.valid == true) {
+        alert("Formulario validado")
+    }
+})
